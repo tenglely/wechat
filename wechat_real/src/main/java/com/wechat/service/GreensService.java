05342f18ec24fb505be2ce3f59276c676d5fae2a
teng <1109927412@qq.com>
2019-03-24 16:31:35 +0800

    Update GreensService.java

--- wechat_real/src/main/java/com/wechat/service/GreensService.java
+++ wechat_real/src/main/java/com/wechat/service/GreensService.java
@@ -1,8 +1,11 @@
 package com.wechat.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.wechat.bean.Greens;
 import com.wechat.dao.GreensMapper;
 
 @Service
@@ -10,4 +13,13 @@ public class GreensService {
 	
 	@Autowired
 	private GreensMapper greensMapper;
+
+	public void addGreens(Greens greens) {
+		greensMapper.insert(greens);
+	}
+
+	public List<Greens> findAll() {
+		
+		return greensMapper.selectByExample(null);
+	}
 }
