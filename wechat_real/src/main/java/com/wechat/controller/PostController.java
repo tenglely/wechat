599dc59a4a014d7ea87efb5ed2030078f6de97ca
itboydeng <48464426+itboydeng@users.noreply.github.com>
2019-04-03 12:55:09 +0800

    更新了postController.java
    更新了postController

--- wechat_real/src/main/java/com/wechat/controller/PostController.java
+++ wechat_real/src/main/java/com/wechat/controller/PostController.java
@@ -1,9 +1,16 @@
 package com.wechat.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.wechat.bean.Msg;
+import com.wechat.bean.Post;
+import com.wechat.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-import com.wechat.service.PostService;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
@@ -24,22 +31,88 @@ public class PostController {
 	 * 2.后台根据贴子编号(pid)删除一条贴子
 	 */
 	//public Msg deletePost()
+	@ResponseBody
+	@RequestMapping(value = "/deletpostbypid/{pids}",method = RequestMethod.DELETE)
+	public Msg deletePostByPid(@PathVariable("pids") String pid){
+		if(pid.contains("-")){
+			List<Integer> del_ids=new ArrayList<>();
+			//批量删除
+			String[] str_ids=pid.split("-");
+			//组装ids的集合
+			for(String string :str_ids){
+				del_ids.add(Integer.parseInt(string));
+			}
+			postService.deleteBatch(del_ids);
+		}else{
+			//单个删除
+			Integer id=Integer.parseInt(pid);
+			postService.deleteone(id);
+		}
+		return Msg.success();
+	}
 	
 	/**
 	 * 3、根据用户编号(uid)查找所有贴子
 	 */
 	//public Msg findPostByUid(int uid)
-	
+	@ResponseBody
+	@RequestMapping(value = "findpostbyuid",method = RequestMethod.GET)
+	public Msg findPostByuid(@RequestParam("uid") String uid){
+		Integer uids=Integer.parseInt(uid);
+		PageHelper.startPage(1, 5);
+		List<Post> posts=postService.getPostByUid(uids);
+		PageInfo page = new PageInfo(posts,5);
+		return Msg.success().add("pageInfo", page);
+	}
+
 	/**
 	 * 4、查找出所有贴子（升序：按最新）(分页)
 	 */
 	//public Msg findAllPost()
-	
-	/**
-	 * 10、按点击量的从高往低查找出所有贴子（分页）
-	 */
-	//public Msg findPostByHot()
-	
+	@ResponseBody
+	@RequestMapping(value = "findallpost",method = RequestMethod.GET)
+	public Msg findAllPost(@RequestParam(value="pn",defaultValue="1") Integer pn,
+						   @RequestParam("select_key") String select_key,
+						   @RequestParam("select_value") String select_value){
+		if(select_key.equals("最多点击")){
+				PageHelper.startPage(pn, 5);
+				List<Post> posts=postService.getAllByHot();
+				PageInfo page=new PageInfo(posts,5);
+				return Msg.success().add("pageInfo", page);
+		}else if(select_key.equals("用户编号")){
+				if(select_value==""){
+					PageHelper.startPage(pn, 5);
+					List<Post> posts=postService.getAll();
+					PageInfo page=new PageInfo(posts,5);
+					return Msg.success().add("pageInfo", page);
+				}else{
+					Integer uids=Integer.parseInt(select_value);
+					PageHelper.startPage(pn, 5);
+					List<Post> posts=postService.getPostByUid(uids);
+					PageInfo page=new PageInfo(posts,5);
+					return Msg.success().add("pageInfo", page);
+				}
+		}else if(select_key.equals("内容")){
+			if(select_value==""){
+				PageHelper.startPage(pn, 5);
+				List<Post> posts=postService.getAll();
+				PageInfo page=new PageInfo(posts,5);
+				return Msg.success().add("pageInfo", page);
+			}else{
+				PageHelper.startPage(pn, 5);
+				List<Post> posts=postService.getPostByPcontent(select_value);
+				PageInfo page=new PageInfo(posts,5);
+				return Msg.success().add("pageInfo", page);
+			}
+		}else{
+			PageHelper.startPage(pn, 5);
+			List<Post> posts=postService.getAll();
+			PageInfo page=new PageInfo(posts,5);
+			return Msg.success().add("pageInfo", page);
+		}
+
+	}
+
 	/**
 	 * 5、根据贴子编号:pid,点击量：hits 数量加1
 	 */
